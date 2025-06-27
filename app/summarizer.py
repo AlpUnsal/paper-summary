14e70fd1bf838c2c5bc8915e1d30f156e8bb0386
Alp Unsal <unsalalp10@gmail.com>
2025-06-27 08:39:51 -0400

    added skeleton for project

---
+++ app/summarizer.py
@@ -0,0 +1,5 @@
+def summarize_section(section_text):
+    pass
+
+def create_insights(summarized_text):
+    pass
---
+++ app/utils.py
@@ -0,0 +1,7 @@
+import pymupdf4llm
+
+def pdf_to_text(pdf_file):
+    pass
+
+def section_spliiter(text):
+    pass
